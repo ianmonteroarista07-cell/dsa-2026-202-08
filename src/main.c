f4927be3ca78aaab5c611ee06948086015e62e8e
danielmiles01-create <daniel.miles01@estudiant.upf.edu>
2026-03-26 10:30:47 +0100

    hitler o algo asi

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int main() {
   printf("*****************\nWelcome to DSA!\n*****************\n");
 
   // how to import and call a function
-  printf("el megafaaactorial of 4 is %d\n", fact(4));
+  printf("el megafaaaccctorial of 4 is %d\n", fact(4));
 
   // uncomment and run "make v" to see how valgrind detects memory leaks
   // createaleak();
